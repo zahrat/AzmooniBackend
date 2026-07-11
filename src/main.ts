9529a011ff399b54fce64166c09997b93f710a9c
zahra <tofighizahra@gmail.com>
2026-07-11 16:39:45 +0330

    add packged

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000, '127.0.0.1');
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
